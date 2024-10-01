10b8545099131a814b47f014e548da9b05dfcbf4
ismaelnjr <ismaelnjr@icloud.com>
2024-09-30 21:33:39 -0300

    v.0.0.7

--- setup.py
+++ setup.py
@@ -3,25 +3,27 @@ from setuptools import find_packages
 
 from spedpytools import __version__
 
-with open("README.md", "r") as arq:
-    readme = arq.read()
+def parse_requirements(filename):
+    with open(filename, encoding='utf-16') as f:
+        return f.read().splitlines()
 
 setup(name='spedpytools',
     version=__version__,
     license='MIT License',
     author='Ismael Nascimento',
-    long_description=readme,
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     author_email='ismaelnjr@icloud.com.br',
     keywords='sped fiscal receita federal',
     description=u'Biblioteca para visualização de um arquivo sped em estrutura de tabelas do Pandas e possibilidade de salvar em formato para excel.',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['spedpy', 'pandas', 'openpyxl', 'tqdm'],
-    classifiers=
-        ["Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"],
-   
-    )
+    install_requires=parse_requirements('requirements.txt'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
 
